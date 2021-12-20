83a2b9afc7dc7a362dc8e8f194a9a939e4dd9125
Olha Gruzglina <olga.zhmura@gmail.com>
2021-12-20 15:10:02 -0500

    Added arrow up (go to the top)

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function clickOnImg(link) {
     location.href = link;
 }
 
+myBtn = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myBtn.style.display = "block";
+      } else {
+        myBtn.style.display = "none";
+      }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function goToTheTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
